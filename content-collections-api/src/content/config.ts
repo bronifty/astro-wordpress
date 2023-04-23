77737fe1f77a90c012baeaa0fe05061820e29a6d
bronifty <bronifty@gmail.com>
2023-04-23 07:37:58 +0000

    this

--- content-collections-api/src/content/config.ts
+++ content-collections-api/src/content/config.ts
@@ -1,7 +1,8 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
+
+const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -19,5 +20,5 @@ const blogCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  blog: blogCollection,
+  blog,
 };
