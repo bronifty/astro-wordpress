3c996be88ac700ee6a696d7e6a35f9fa802b0583
bronifty <bronifty@gmail.com>
2023-04-24 00:00:16 +0000

    this

--- content-collections-api/src/js/utils.js
+++ content-collections-api/src/js/utils.js
@@ -1,3 +1,20 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString("en-US", {
+    timeZone: "UTC",
+  });
+}
+
 export function formatBlogPosts(
   posts,
   {
