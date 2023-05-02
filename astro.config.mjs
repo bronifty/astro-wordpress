2e39e728759ab07a4b10a86444400a764f667929
bronifty <bronifty@gmail.com>
2023-05-02 03:41:14 +0000

    added search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()]
+  // output: "server",
+  site: "https://blog.bronifty.xyz",
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+    sitemap(),
+  ],
+  // adapter: vercel(),
 });
