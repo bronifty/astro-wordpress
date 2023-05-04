d3476986115800f8ae87b542e0faa79bb4dcdfc3
JLarky <jlarky@gmail.com>
2023-05-03 20:25:31 -0600

    refactor getPosts function

--- src/pages/slugs.json.ts
+++ src/pages/slugs.json.ts
@@ -1,21 +1,23 @@
 import { getCollection } from "astro:content";
 import type { APIRoute } from "astro";
 
-export const get: APIRoute = async function get() {
+export async function getPosts() {
   const posts = await getCollection("blog");
 
-  const body = JSON.stringify({
-    posts: posts.map((post) => ({
-      title: post.data.title,
-      author: post.data.author,
-      category: post.data.category,
-      date: post.data.date,
-      description: post.data.description,
-      content: post.body,
-      slug: `/blog/${post.slug}`,
-    })),
-  });
+  return posts.map((post) => ({
+    title: post.data.title,
+    author: post.data.author,
+    category: post.data.category,
+    date: post.data.date.toString(),
+    description: post.data.description,
+    content: post.body,
+    slug: `/blog/${post.slug}`,
+  }));
+}
 
+export const get: APIRoute = async function get() {
+  const posts = await getPosts();
+  const body = JSON.stringify({ posts });
   return {
     body,
   };
