44a6351868c2c9a94e3eb7532e7506d211eb1374
bronifty <bronifty@gmail.com>
2023-05-04 17:59:56 +0000

    ?

--- src/components/ShowPosts.tsx
+++ src/components/ShowPosts.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { useStore } from "@nanostores/react";
-import { postsArray, filteredPosts } from "../store/posts";
+import { Post, postsArray, searchTerm, filterPosts } from "../store/posts";
 import PostCard from "./PostCard";
 
 const ShowPosts = () => {
+  // import loaded postsArray and make it reactive inside the component
   const postsStore = useStore(postsArray);
-  const filteredPostsStore = useStore(filteredPosts);
+  // import searchTerm and make it reactive inside the component
+  // whenever searchTerm changes, the component will re-render
+  const searchTermStore = useStore(searchTerm);
+  // when the component re-renders, the value of posts will be reset to the return value of filterPosts, which is loaded with the postsArray (retrieved from the slugs.json file) and filtered by the current value of searchTerm; if searchTerm is empty, we make sure to not return any posts;
+  const posts = filterPosts(postsStore, searchTermStore);
   return (
     <>
-      {filteredPosts.get().length > 0 && (
+      {posts.length > 0 && (
         <div>
           <h1>Filtered Posts</h1>
           <ul>
-            {filteredPosts.get().length > 0 &&
-              filteredPosts.get().map((post, idx) => (
-                <li key={idx}>
-                  <PostCard
-                    title={post.title}
-                    author={post.author}
-                    category={post.category}
-                    date={post.date}
-                    description={post.description}
-                    slug={post.slug}
-                  />
-                </li>
-              ))}
+            {posts.map((post, idx) => (
+              <li key={idx}>
+                <PostCard
+                  title={post.title}
+                  author={post.author}
+                  category={post.category}
+                  date={post.date}
+                  description={post.description}
+                  slug={post.slug}
+                />
+              </li>
+            ))}
           </ul>
         </div>
       )}
--- src/store/posts.ts
+++ src/store/posts.ts
@@ -14,20 +14,62 @@ export interface Post {
 export const postsArray = atom<Post[]>([]);
 export const searchTerm = atom<string>("");
 
-export const filteredPosts = computed(searchTerm, (search) => {
-  if (search === "") {
-    return postsArray.get().filter((post) => post.title === "");
+// load the postsArray when we are on the client
+// canonical astro code is: if (!import.meta.env.SSR) { ... }
+if (typeof window !== "undefined") {
+  (async () => {
+    const res = await fetch("/slugs.json");
+    const data = await res.json();
+    return postsArray.set(data.posts);
+  })();
+}
+
+// filter the postsArray
+export const filterPosts = (posts: Post[], searchTerm: string) => {
+  if (searchTerm === "") {
+    return posts.filter((post) => post.title === "");
   }
-  return postsArray
-    .get()
-    .filter(
-      (post) =>
-        post.title.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
-        post.description
-          .toLowerCase()
-          .includes(searchTerm.get().toLowerCase()) ||
-        post.content.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
-        post.author.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
-        post.category.toLowerCase().includes(searchTerm.get().toLowerCase())
+  return posts.filter((post) => {
+    const titleMatch = post.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const contentMatch = post.content
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const authorMatch = post.author
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const categoryMatch = post.category
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const descriptionMatch = post.description
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    return (
+      titleMatch ||
+      contentMatch ||
+      authorMatch ||
+      categoryMatch ||
+      descriptionMatch
     );
-});
+  });
+};
+
+// redundant but useful code for another possible case; computed derived values
+// export const filteredPosts = computed(searchTerm, (search) => {
+//   if (search === "") {
+//     return postsArray.get().filter((post) => post.title === "");
+//   }
+//   return postsArray
+//     .get()
+//     .filter(
+//       (post) =>
+//         post.title.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
+//         post.description
+//           .toLowerCase()
+//           .includes(searchTerm.get().toLowerCase()) ||
+//         post.content.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
+//         post.author.toLowerCase().includes(searchTerm.get().toLowerCase()) ||
+//         post.category.toLowerCase().includes(searchTerm.get().toLowerCase())
+//     );
+// });
