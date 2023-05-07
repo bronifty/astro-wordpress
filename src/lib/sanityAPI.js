3a196cec9a9b3fd754c0bc685e370a6f0e6cec47
bronifty <bronifty@gmail.com>
2023-05-07 07:19:06 +0000

    updated secrets

--- src/lib/sanityAPI.js
+++ src/lib/sanityAPI.js
@@ -1,4 +1,7 @@
-let PROJECT_ID = "bpy0mxas";
+import dotenv from "dotenv";
+dotenv.config();
+const SANITY_PROJECT_ID = process.env.SANITY_PROJECT_ID;
+let PROJECT_ID = SANITY_PROJECT_ID;
 let DATASET = "production";
 let QUERY = encodeURIComponent('*[_type == "pet"]');
 let PROJECT_URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
